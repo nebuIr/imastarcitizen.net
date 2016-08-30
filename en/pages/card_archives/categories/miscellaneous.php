93060cc6a624abc7a7cc2e008952d2970188a97f
xnebulr <prime@nebulr.org>
2016-08-30 20:02:59 +0200

    Added multlianguage support using PHP (de, en)
    * No duplicated site anymore
    * Deleted 'de' folder

--- en/pages/card_archives/categories/miscellaneous.php
+++
@@ -1,8 +0,0 @@
-<h1>Miscellaneous</h1>
-<?php include('plugins/dropdown/dropdown_miscellaneous.php'); ?>
-    <div id="cards">
-        <div class="cards_box">
-            <a class="fancybox" rel="cards" href="../images/cards/cards_full_res/1-100/1/1.jpg"><img class="shadow-cards" src="../images/cards/cards_small/1-100/1/1.jpg" id="No. 1" width="210" height="295" /></a>
-            <a class="fancybox" rel="cards" href="../images/cards/cards_full_res/1-100/1/1b.jpg"><img class="shadow-cards" src="../images/cards/cards_small/1-100/1/1b.jpg" id="No. 1b" width="210" height="295" /></a>
-        </div>
-        </br>
---
+++ pages/downloads.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
+    <title></title>
+</head>
+
+<body>
+    <div id=downloads>
+        <h1>PDF Download</h1>
+        <p><?php echo $lang['DOWNLOADS_UPDATE']; ?></p>
+        <a id="button" href="../downloads/cards_print/cards_print.pdf"><i class="fa fa-print"></i>
+        <?php echo $lang['DOWNLOADS_BUTTON']; ?></a>
+        <br>
+        <!-- <p><b>Missing cards</b> <i>(I'll add them soon)</i>:</p></br>
+        <ul>
+            <li>18 - Randy Vazquez</li>
+        </ul> -->
+    </div><img height="512" width="680" src="../images/preview/cards_download_preview_small.png">
+</body>
+
+</html>
