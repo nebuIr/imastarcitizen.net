8a27e2487b570d7f17cd98f0d6d7dc6a34f79bc6
nebuIr <brendlifabian@live.de>
2016-09-03 17:45:17 +0200

    Fixed PHP session error
    Fixed PHP session error, not being able to change the language

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php include('locale/locale.php'); ?>
 <html>
 
 <head>
@@ -9,7 +10,6 @@
     <link href="../style/css/footer.css" rel="stylesheet">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
     <script src='https://www.google.com/recaptcha/api.js'></script>
-    <?php include('locale/locale.php'); ?>
     <?php
 
         if(isset($_GET["section"]))
